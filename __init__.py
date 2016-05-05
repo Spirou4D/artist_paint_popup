3a1c8be0d7e39c2c14706ed4921eb7708599ee96
Jean Sipren <Spirou4D@users.noreply.github.com>
2016-05-05 22:14:35 +0200

    Update_047
    Changed the UI with both view mode buttons.

--- __init__.py
+++ __init__.py
@@ -51,6 +51,23 @@ def get_addon_preferences():
     addon_preferences = user_preferences.addons['artist_paint_panel'].preferences
     return addon_preferences
 
+def pollAPT(self, context):
+    scene = context.scene
+    obj =  context.active_object
+    empty = scene.maincanvas_is_empty
+    main_canvas_name = ""
+
+    if not(empty):
+        if scene.artist_paint is not None:
+            if len(scene.artist_paint) !=0:
+                for main_canvas in scene.artist_paint:
+                    main_canvas_name = (main_canvas.filename)[:-4]
+    else:
+        return False
+
+    if obj is not None:
+        return obj.name == main_canvas_name
+
 class canvasPopup(Operator):
     bl_idname = "artist_paint.popup"
     bl_label = "Artist Paint Popup"
@@ -75,20 +92,34 @@ class canvasPopup(Operator):
     def draw(self, context):
         #"ARTIST_PAINT_OT_popup"
         addon_prefs = get_addon_preferences()
+        buttName_1 = str(addon_prefs.customAngle) +"°"
+        buttName_2 = str(addon_prefs.customAngle) +"°"
+
         CustomAngle  = str(addon_prefs.customAngle)
         tool_settings = context.tool_settings
         ipaint = context.tool_settings.image_paint
 
         layout = self.layout
         layout.active = context.scene.UI_is_activated
-        trunk = layout.column()
-        trunk.separator()
-        trunk.label("Objects Masking Tools")
-        box = trunk.box()
+
+        row = layout.row(align = True)
+        row1 = row.split(align=True)
+        row1.label("Shading")
+        row2 = row.split(align=True)
+        row2.operator("artist_paint.multitexture",
+                    text='', icon="RENDERLAYERS")
+        row2.operator("artist_paint.glsl",
+                    text='', icon="RADIO")
+        row2.scale_x = 1.00
+
+        box = layout.box()
         col = box.column()
+        col.label("Objects Masking Tools")
         col.operator("artist_paint.trace_selection",
                     text = "Mask from Gpencil",
                     icon = 'OUTLINER_OB_MESH')
+
+        col.separator()                             #empty line
 
         row = col.row(align=True)
         row.operator("artist_paint.curve_2dpoly",
@@ -97,51 +128,63 @@ class canvasPopup(Operator):
         row.operator("artist_paint.curve_unwrap",
                     text = "",
                     icon = 'OUTLINER_OB_MESH')
+
         col.operator("artist_paint.inverted_mask",
                     text = "Mesh Mask Inversion",
                     icon = 'MOD_TRIANGULATE')
 
+        col.separator()                             #empty line
+
         col.prop(ipaint, "use_stencil_layer",
                                 text="Use stencil mask")
-
         if ipaint.use_stencil_layer == True:
-            col.template_ID(ipaint, "stencil_image")
-            col.operator("image.new", text="New").\
-                                gen_context = 'PAINT_STENCIL'
-            row = col.row(align = True)
+            cel = col.column(align = True)
+            cel.template_ID(ipaint, "stencil_image")
+            cel.operator("image.new", text="New").\
+                                        gen_context = 'PAINT_STENCIL'
+            row = cel.row(align = True)
             row.prop(ipaint, "stencil_color", text="")
             row.prop(ipaint, "invert_stencil",
                         text="Invert the mask",
                         icon='IMAGE_ALPHA')
-            #col.prop(ipaint, "invert_stencil", text="Invert the mask")
 
-        trunk.separator()                             #empty line
-        trunk.label("Mirrors / Rotations")
-        box = trunk.box()
-        col = box.column(align = True)
-        col.prop(context.scene, "canvas_in_frame" ,
-                                    text="Canvas Frame Constraint")
-        row = col.row(align=True)
-        row.operator("artist_paint.canvas_horizontal",
-                text="Flip Horizontal",icon='ARROW_LEFTRIGHT')
-        row.operator("artist_paint.canvas_vertical",
-                text = "Flip Vertical", icon = 'FILE_PARENT')
-        row = col.row(align=True)
         col.separator()                             #empty line
-        row = col.row(align=True)
-        buttName_1 = "Rotate " + CustomAngle +"° CCW"
-        buttName_2 = "-"+buttName_1
+
+        box = layout.box()
+        col = box.column(align = True)          #CANVAS FRAME CONSTRAINT
+        row = col.row(align = True)
+        row.label(text="Mirror")                      #MIRROR FLIP
+        row.enabled = pollAPT(self, context)
+
+        row = col.row(align = True)
+        row.operator("artist_paint.canvas_horizontal",
+                    text="Canvas Flip Horizontal",
+                    icon='ARROW_LEFTRIGHT')
+        row.operator("artist_paint.canvas_vertical",
+                    text = "Canvas Flip Vertical",
+                    icon = 'FILE_PARENT')
+
+
+        row = col.row(align = True)                    #ROTATION
+        row.label(text="Rotation")
+        row.prop(context.scene, "canvas_in_frame" ,
+                                    text="Frame Constraint")
+        row.enabled = pollAPT(self, context)
+
+        row = col.row(align = True)
         row.operator("artist_paint.rotate_ccw_15",
-                text = buttName_1, icon = 'TRIA_LEFT')
+                    text = "Rotate -" + buttName_1, icon = 'TRIA_LEFT')
         row.operator("artist_paint.rotate_cw_15",
-                text = buttName_2, icon = 'TRIA_RIGHT')
-        row = col.row(align=True)
+                    text = "Rotate +" + buttName_2, icon = 'TRIA_RIGHT')
+
+        row = col.row(align = True)
         row.operator("artist_paint.rotate_ccw_90",
-                text = "Rotate 90° CCW", icon = 'PREV_KEYFRAME')
+                    text = "Rotate 90° CCW", icon = 'PREV_KEYFRAME')
         row.operator("artist_paint.rotate_cw_90",
-                text = "Rotate 90° CW", icon = 'NEXT_KEYFRAME')
+                    text = "Rotate 90° CW", icon = 'NEXT_KEYFRAME')
+
         col.operator("artist_paint.canvas_resetrot",
-                text = "Reset Rotation", icon = 'CANCEL')
+                    text = "Reset Rotation", icon = 'CANCEL')
 
 
 
